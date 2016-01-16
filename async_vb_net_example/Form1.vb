411981e1c6ac76f8e221caff947d59520e31ef6a
Ruslan Shaydulin <ruslan@l2sh.ru>
2016-01-16 12:39:28 +0300

    Initial commit
    Button 2 works asynchronously, Button 1 – does not. Press button 3 to
    check!

---
+++ async_vb_net_example/Form1.vb
@@ -0,0 +1,34 @@
+﻿
+
+Public Class Form1
+    Sub LongOperation()
+
+        Threading.Thread.Sleep(5000)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Button 1 in charge"
+        Application.DoEvents()  ' needed here so that the label will update
+
+        LongOperation()
+        Label1.Text = "Button 1 finished"
+    End Sub
+
+    Private Async Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = "Button 2 in charge"
+        Application.DoEvents()  ' needed here so that the label will update
+
+        Await Task.Run(Sub()
+                           LongOperation()
+                       End Sub)
+
+        Label1.Text = "Button 2 finished"
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label2.Text = "it's okay!"
+        Application.DoEvents()  ' needed here so that the label will update
+    End Sub
+End Class
---
+++ async_vb_net_example/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("async_vb_net_example")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("async_vb_net_example")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("50ec731f-39d6-4b65-b2f1-432b9b587228")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
